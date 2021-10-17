39894190b92d1620623685998c0647fac2b9ec4f
miuramiure <account@m-miura.jp>
2021-10-17 21:34:08 +0900

    グラフオブジェクトの生成処理を追加、隣接行列を返す関数を追加など

--- class/BalusModel.m
+++ class/BalusModel.m
@@ -13,7 +13,7 @@ classdef BalusModel < handle
     end
     methods
         function obj = BalusModel(jsonText)
-            %
+            %jsonの構造をMatlab構造体に変換
             model = jsondecode(jsonText);
             obj.application = model.application;
             obj.target = model.target;
@@ -31,20 +31,56 @@ classdef BalusModel < handle
             [N,~] = size(nodes);%ノード数
             
             %ノードのkeyを格納した配列
-            nodeKeys = strings(N,1);
+            nodeKey = strings(N,1);
             for i=1:N
-                nodeKeys(i) = string(nodes(i).key);
+                nodeKey(i) = string(nodes(i).key);
             end
             
             %digraphを生成
             fromNodeIDs = zeros(1,L);
             toNodeIDs = zeros(1,L);
             for i=1:L
-                fromNodeIDs(i) = getIndexOfMatch(nodeKeys,string(links(i).fromKey));%fromKeyと一致するノードの番号（ID）
-                toNodeIDs(i) = getIndexOfMatch(nodeKeys,string(links(i).toKey));%toKeyと一致するノードの番号（ID）
+                fromNodeIDs(i) = getIndexOfMatch(nodeKey,string(links(i).fromKey));%fromKeyと一致するノードの番号（ID）
+                toNodeIDs(i) = getIndexOfMatch(nodeKey,string(links(i).toKey));%toKeyと一致するノードの番号（ID）
             end
             g = digraph(fromNodeIDs, toNodeIDs);
             
+            
+            %ノード情報テーブルに情報を追加
+            nodeText = strings(N,1);%テキスト
+            nodePositionX = zeros(N,1);%x座標
+            nodePositionY = zeros(N,1);%y座標
+            nodeColor = strings(N,1);%themeColor
+            
+            for i=1:N
+                nodeText(i) = string(nodes(i).name.value);
+                nodePositionX(i) = nodes(i).position.x;
+                nodePositionY(i) = nodes(i).position.y;
+                nodeColor(i) = string(nodes(i).style.themeColor);
+            end
+            
+            g.Nodes.text = nodeText;
+            g.Nodes.x = nodePositionX;
+            g.Nodes.y = nodePositionY;
+            g.Nodes.color = nodeColor;
+            
+            
+            %エッジ情報テーブルに情報を追加
+            linkText = strings(N,1);%テキスト
+            linkLine = strings(N,1);%x座標
+            linkMark = strings(N,1);%x座標      
+            
+            for i=1:L
+                linkText(i) = string(links(i).name.value);
+                linkLine(i) = string(links(i).style.lineStyle);
+                linkMark(i) = string(links(i).style.markStyle);
+            end
+            
+            g.Edges.text = linkText;
+            g.Edges.line = linkLine;
+            g.Edges.mark = linkMark;
+            
+            %グラフオブジェクトを生成して戻り値へ
             graph = Graph(g,1);
             
         end
--- class/Graph.m
+++ class/Graph.m
@@ -12,16 +12,25 @@ classdef Graph < handle
     end   
     methods
         function obj = Graph(graph, directed)
-            
+            %コンストラクタ
             obj.graph = graph;
             %graphが無向の場合はdirected = 0, 有向の場合は directed = 1
             obj.directed = directed;
         end
+              
+        function val = get.nodeNum(obj)
+            %ノード数のゲットメソッド
+            val = numnodes(obj.graph);
+        end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 このメソッドの概要をここに記述
-            %   詳細説明をここに記述
-            outputArg = obj.Property1 + inputArg;
+        function val = get.linkNum(obj)
+            %ノード数のゲットメソッド
+            val = numedges(obj.graph);
+        end
+        
+        function adj = getAdjacency(obj)
+            %隣接行列（フル行列表記）
+            adj = full(adjacency(obj.graph));
         end
     end
 end
